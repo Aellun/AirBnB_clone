4ed29941719712b3797d787199528789a110e0d1
Aellun <okello.k.otieono@gmail.com>
2023-11-18 20:23:31 -0800

    fixed pycodestyle requirement

--- models/place.py
+++ models/place.py
@@ -7,17 +7,17 @@ class Place(BaseModel):
     """Represent a place.
 
     Attributes:
-        city_id (str): The City id.
-        user_id (str): The User id.
-        name (str): The place name.
+        city_id (str): The City id
+        user_id (str): The User id
+        name (str): The place name
         description (str): The  place description
-        number_rooms (int): The place number of rooms 
-        number_bathrooms (int): The place number of bathrooms 
-        max_guest (int): The place maximum number of guests
-        price_by_night (int): The place price by night
-        latitude (float): The place latitude
-        longitude (float): The  place longitude
-        amenity_ids (list): A list of Amenity ids.
+            number_rooms (int): The place number of rooms
+            number_bathrooms (int): The place number of bathrooms
+            max_guest (int): The place maximum number of guests
+            price_by_night (int): The place price by night
+            latitude (float): The place latitude
+            longitude (float): The  place longitude
+            amenity_ids (list): A list of Amenity ids.
     """
 
     city_id = ""
